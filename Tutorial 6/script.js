420a537a1d2d77a7e022e0b2b5d4216ab5f977b2
Ravi yadav <yadavravi7770@gmail.com>
2021-06-05 17:11:13 +0530

    string template
    Signed-off-by: Ravi yadav <yadavravi7770@gmail.com>

---
+++ Tutorial 6/script.js
@@ -0,0 +1,5 @@
+  function logname(name ,age){
+      console.log(`My name is ${name}and My age is ${age}`);/*we are using string template*/ 
+  }
+  logname("rhu",23); /*the output of the fllowing code is simple even if are changing like get extra space cannot get counted*/ 
+

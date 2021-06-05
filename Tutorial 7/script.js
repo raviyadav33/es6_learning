a618172de58349cc644bdf6a036231ea4a7145c7
Ravi yadav <yadavravi7770@gmail.com>
2021-06-05 17:27:09 +0530

    string method
    Signed-off-by: Ravi yadav <yadavravi7770@gmail.com>

---
+++ Tutorial 7/script.js
@@ -0,0 +1,14 @@
+/*New Method
+  repeat
+  startswith
+  endswith
+  includes
+*/ 
+
+var str="graaaaaaaaaaaaavy"
+
+console.log(str.repeat(4))/*output 4*graaaavy*/
+
+console.log(str.startsWith("gra")) ;/*true*/ 
+console.log(str.startsWith("avy"));/*output is false bcz it not start with avy*/
+console.log(str.endsWith("gra")) ;/*false*/ 

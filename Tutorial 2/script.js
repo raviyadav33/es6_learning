aa867f55dc7f5f8a32d753d4995505b1ec053918
Ravi yadav <yadavravi7770@gmail.com>
2021-06-05 15:54:12 +0530

    es6
    Signed-off-by: Ravi yadav <yadavravi7770@gmail.com>

---
+++ Tutorial 2/script.js
@@ -0,0 +1,10 @@
+window .onload=function(){
+    /*Const means we assing a value we cannot change the new value /set the new value */
+    const pi=3.14; 
+    // console .log(pi);
+    /*lets try to calculate the area of circle*/
+    function calArea(r){
+        console.log('Area of the circle :'+pi*r*r);
+    } 
+    calArea();
+}

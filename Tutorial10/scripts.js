0666c4cdc093c584b3c2389011977d3d16b56b6e
Ravi yadav <yadavravi7770@gmail.com>
2021-06-05 18:39:38 +0530

    sets
    Signed-off-by: Ravi yadav <yadavravi7770@gmail.com>

---
+++ Tutorial10/scripts.js
@@ -0,0 +1,8 @@
+var names =new Set();
+
+names.add("shaun").add("mari").add("kane");
+
+
+console.log(names);  /*o/p sets{"shaun","mari","kane"}*/
+console.log(names.size);
+console.log(names.delete("shaun"));
